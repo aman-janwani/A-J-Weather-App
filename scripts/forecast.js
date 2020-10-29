402dc0e13b44188855ed4be3a43583c1a69b6631
aman-janwani <69379499+aman-janwani@users.noreply.github.com>
2020-10-29 16:50:14 +0530

    8

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,6 @@
 class Forecast{
     constructor(){
-        this.key = "pTmO5dtEQciTdAQ1OPnujpU4HMFZye3l";
+        this.key = "nQlNopQHrWPAZ5NOKBiAXNPs9FCuyFeG";
         this.weatherURI = "https://dataservice.accuweather.com/currentconditions/v1/";
         this.cityURI = "https://dataservice.accuweather.com/locations/v1/cities/search";
 
