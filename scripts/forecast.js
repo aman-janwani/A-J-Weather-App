84798135ffcc803219661f2761f7d38977d0c45b
aman-janwani <69379499+aman-janwani@users.noreply.github.com>
2021-06-09 13:17:08 +0530

    KEY

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,6 +1,6 @@
 class Forecast{
     constructor(){
-        this.key = "${{secret.SECRET_KEY}}";
+        this.key = `${{secret.SECRET_KEY}}`;
         this.weatherURI = "https://dataservice.accuweather.com/currentconditions/v1/";
         this.cityURI = "https://dataservice.accuweather.com/locations/v1/cities/search";
 
